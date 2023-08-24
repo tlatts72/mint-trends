c34c5c0fbff509f380e767356586f7810db2cf9e
Tim <tlatts72@gmail.com>
2023-08-24 09:01:15 -0500

    corrections

--- db_update.py
+++ db_update.py
@@ -1,8 +1,9 @@
 import mintapi
-from config import accounts
+from config import accounts, totp_secret
 from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pyotp
 
 # Set up Chrome options for headless browsing
 chrome_options = Options()
@@ -20,11 +21,23 @@ collection = db['account_balances']
 
 # Loop through each email account and query account balances
 for email, passwd in accounts.items():
-    mint = mintapi.Mint(email=email, password=passwd, driver=driver)
+    # Get the Authy token
+    token = pyotp.TOTP(totp_secret).now()
+    print(token)
+
+    # Authenticate with Mint using the Authy token
+    mint = mintapi.Mint(
+        email=email,
+        password=passwd,
+        mfa_method="soft-token",
+        mfa_token=totp_secret,
+        driver=driver
+    )
 
     # Get all account balances
     balances = mint.get_accounts()
     print(balances)
+
     # Insert the account balances into MongoDB
     for balance in balances:
         collection.insert_one(balance)
