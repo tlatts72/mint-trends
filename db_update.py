cc0ad80b873902555c4346be115b2ed8edee74d4
Lattimer, Tim <Tim.Lattimer@charter.com>
2023-08-31 10:36:58 -0500

    db_update changes - should work for cc accounts

--- db_update.py
+++ db_update.py
@@ -1,60 +1,62 @@
-import mintapi as mint
+import mintapi
 from datetime import datetime
 from config import accounts as config_accounts, mongo_credentials
 from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Set up Chrome options for headless browsing
-chrome_options = Options()
-chrome_options.add_argument("headless=new")
-
-# Create a new Chrome web driver with the headless option
-driver = webdriver.Chrome(options=chrome_options)
-
-# Connect to MongoDB
-client = MongoClient(host='mongodb://localhost:27017/', username=mongo_credentials["User"],
-                     password=mongo_credentials["Password"])
-db = client['mint_trends']
-collection = db['account_balances']
-
 
 def validate_account(account):
     if account["accountStatus"] == "CLOSED":
         return False
     else:
-        return True
+        if account["Whitelist"]:
+            if len(account["Whitelist"]) > 0:
+                # add logic
+                return True
+            else:
+                return True
+        else:
+            return True
 
-def mint_pull():
+
+def mint_pull(credentials):
+    # Set up Chrome options for headless browsing
+    chrome_options = Options()
+    chrome_options.add_argument("headless=new")
+    # Create a new Chrome web driver with the headless option
+    driver = webdriver.Chrome(options=chrome_options)
+    mint = mintapi.Mint(credentials["User"], credentials["Password"], mfa_method="soft-token",
+                        mfa_token=credentials["TOTP"], wait_for_sync=False, driver=driver)
     balances = mint.get_account_data()
+    mint.close()
+    # Quit the Chrome web driver
+    driver.quit()
     print(balances)
 
 
 def mongo_upload():
+    # Connect to MongoDB "mint_trends" with collection "account_balances"
+    client = MongoClient(host='mongodb://localhost:27017/', username=mongo_credentials["User"],
+                         password=mongo_credentials["Password"])
+    db = client['mint_trends']
+    collection = db['account_balances']
     # Loop through each email account and query account balances
     for each in config_accounts:
-        mint_pull()
+        mint_pull(each)
         if validate_account(each):
             if each["Type"] == "CreditAccount":
-                insert = {"Type": each["Type"], "Balance": each["currentBalance"], "Available": each["availableCredit"],
-                          "Mint Last Update": each["lastUpdatedDate"], "Insert Time": datetime.now()
-                          }
-            # elif each["Type"] == "BankAccount":
-
-
-        # Insert the account balances into MongoDB
-        for balance in balances:
-            collection.insert_one(balance)
-
-        # Close the Mint connection
-        mint.close()
+                insert_statement = {"Type": each["Type"], "Balance": each["currentBalance"],
+                                    "Available": each["availableCredit"],
+                                    "Mint_Last_Update": each["lastUpdatedDate"], "Insert Time": datetime.now()
+                                    }
+                collection.insert_one(insert_statement)
+            else:
+                pass
 
     # Close the MongoDB connection
     client.close()
 
-    # Quit the Chrome web driver
-    driver.quit()
-
 
 if __name__ == "__main__":
-    data_upload()
+    mongo_upload()
